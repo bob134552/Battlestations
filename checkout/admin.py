02cceaeeb2a7630104952954c382c060b90a444c
bob134552 <bob134552@hotmail.com>
2021-05-05 17:06:34 +0000

    Updated webhooks, removed copper-spinner.gif, updated checkout form.

--- checkout/admin.py
+++ checkout/admin.py
@@ -12,12 +12,14 @@ class OrderAdmin(admin.ModelAdmin):
 
     readonly_fields = ('order_number', 'date',
                        'delivery_cost', 'order_total',
-                       'final_amount',)
+                       'final_amount', 'original_basket',
+                       'stripe_pid')
 
     fields = ('order_number', 'date', 'full_name', 'email',
               'phone_number', 'country', 'postcode', 'town_or_city',
               'street_address1', 'street_address2', 'county', 'delivery_cost',
-              'order_total', 'final_amount',)
+              'order_total', 'final_amount', 'original_basket',
+              'stripe_pid')
 
     list_display = ('order_number', 'date', 'full_name',
                     'delivery_cost', 'order_total', 'final_amount',)
--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -62,7 +62,7 @@ form.addEventListener('submit', function (ev) {
                     name: $.trim(form.full_name.value),
                     phone: $.trim(form.phone_number.value),
                     email: $.trim(form.email.value),
-                    address: {
+                    address:{
                         line1: $.trim(form.street_address1.value),
                         line2: $.trim(form.street_address2.value),
                         city: $.trim(form.town_or_city.value),
